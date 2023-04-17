1b07002c69d19c04b7977a1aac115e2eb22cff28
Felix Wiemann <13033994+FelixWiemann@users.noreply.github.com>
2023-04-17 19:35:42 +0200

    renamed angular project
    to better reflect that more features than just weather is displayed

--- display/karma.conf.js
+++ display/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/weather'),
+      dir: require('path').join(__dirname, './coverage/display'),
       subdir: '.',
       reporters: [
         { type: 'html' },
